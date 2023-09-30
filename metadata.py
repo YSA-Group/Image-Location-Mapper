66dcc9c57076e8dd59307a749c81a2bf944461d0
Syed Asef Hussain <81021961+Syed-Asef@users.noreply.github.com>
2023-09-30 17:10:47 +0100

    Converted to Class for Object functionality
    Converted the code into a class so objects can be created for each image and stores the date created as well.

--- metadata.py
+++ metadata.py
@@ -1,29 +1,27 @@
 import PIL.Image
-
-img = PIL.Image.open("test.jpg")
-
 import PIL.ExifTags
-exif = {
-    PIL.ExifTags.TAGS[k]: v
-    for k, v in img._getexif().items()
-    if k in PIL.ExifTags.TAGS 
-}
 
-latPre = exif['GPSInfo'][2]
-lonPre = exif['GPSInfo'][4]
+class image:
+    def __init__(self, imgDir):
+        self.img = PIL.Image.open(imgDir)
+        self.exif = {
+            PIL.ExifTags.TAGS[k]: v
+            for k, v in self.img._getexif().items()
+            if k in PIL.ExifTags.TAGS 
+        }
 
-print(exif['GPSInfo'])
-print(latPre)
-print(lonPre)
+        self.latPre = self.exif['GPSInfo'][2]
+        self.lonPre = self.exif['GPSInfo'][4]
 
+        self.latitude = float(((((self.latPre[0] * 60) + self.latPre[1])* 60 ) + self.latPre[2]) / 60 / 60)
+        if self.exif['GPSInfo'][1] == 'S':
+            self.latitude *= -1
 
-latitude = float(((((latPre[0] * 60) + latPre[1])* 60 ) + latPre[2]) / 60 / 60)
-longitude = float(((((lonPre[0] * 60) + lonPre[1])* 60 ) + lonPre[2]) / 60 / 60)
+        self.longitude = float(((((self.lonPre[0] * 60) + self.lonPre[1])* 60 ) + self.lonPre[2]) / 60 / 60)
+        if self.exif['GPSInfo'][3] == 'W':
+            self.longitude *= -1
+        
+        self.dateCreated = self.exif["DateTimeOriginal"]
 
-if exif['GPSInfo'][1] == 'S':
-    latitude *= -1
-if exif['GPSInfo'][3] == 'W':
-    longitude *= -1
-
-print(latitude)
-print(longitude)
+        
+test = image("test.jpg")
